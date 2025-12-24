550dbc8e2b604c9a0ff825a99364f4627a763d5d
Gizzmonauta <gizzmonauta@hotmail.com>
2025-12-24 03:08:04 -0500

    added delta time

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -16,6 +18,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000.0
+        print(f"Delta time: {dt} seconds")
 
     
 
