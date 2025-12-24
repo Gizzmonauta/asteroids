aa4660a1afced42a96fff872835097c1bdde5f59
Gizzmonauta <gizzmonauta@hotmail.com>
2025-12-24 05:08:33 -0500

    added a triangle in the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,18 +12,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pyr = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        pyr.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick() / 1000.0
         print(f"Delta time: {dt} seconds")
 
-    
+
 
 if __name__ == "__main__":
     main()
